74316f3f40ff6e886a9304aaccfdbfe1e57a438b
jeffprakash <jeffprakash@gmail.com>
2022-12-29 14:43:01 +0530

    latest3

--- fishup/api/urls.py
+++ fishup/api/urls.py
@@ -2,20 +2,17 @@ from . import views
 from django.urls import path
 
 
+
 urlpatterns = [     
     
-    path("signup-customer/",views.customercreate(),name="signup-owner"),
-    path("signup-fisherman/",views.fishermancreate(),name="signup-fisherman"),
-    path("signup-retailer/",views.retailerscreate(),name="signup-retailer"),
-    path("addfish/",views.fishcreate(),name="addfish"),
-    path("listfish/",views.fishlist(),name="listfish"),
-    path("listcustomer/",views.customerlist(),name="listcustomer"),
-    path("listretailer/",views.retailerslist(),name="listretailer"),
-    path("listfisherman/",views.fishermanlist(),name="listfisherman"),
-    
-
-    
-
+    path("signup-customer/",views.customercreate.as_view(),name="signup-owner"),
+    path("signup-fisherman/",views.fishermancreate.as_view(),name="signup-fisherman"),
+    path("signup-retailer/",views.retailerscreate.as_view(),name="signup-retailer"),
+    path("addfish/",views.fishcreate.as_view(),name="addfish"),
+    path("listfish/",views.fishlist.as_view(),name="listfish"),
+    path("listcustomer/",views.customerlist.as_view(),name="listcustomer"),
+    path("listretailer/",views.retailerslist.as_view(),name="listretailer"),
+    path("listfisherman/",views.fishermanlist.as_view(),name="listfisherman"),
 
 
 ]
--- fishup/fishup/settings.py
+++ fishup/fishup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,6 +132,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"        
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 REST_FRAMEWORK = {  
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]

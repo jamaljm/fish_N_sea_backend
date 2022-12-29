274ef7bc4f6d86a7ec94496bb0de5e6501d7df22
jeffprakash <jeffprakash@gmail.com>
2022-12-29 12:48:42 +0530

    table

--- fishup/api/models.py
+++ fishup/api/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+
+
+class customer(models.Model):
+    name = models.TextField(blank=False)
+    email = models.EmailField(blank=True,null=True)
+    location= models.TextField(blank=True,null=True)
+    password = models.CharField(max_length=100,blank=True,null=True)
+    password2 = models.CharField(max_length=100,blank=True,null=True)
+
+    def __str__(self):
+        return self.name  
+
+
+
+
+
+class retailers(models.Model):
+    name = models.TextField(blank=False)
+    email = models.EmailField(blank=True,null=True)
+    location= models.TextField(blank=True,null=True)
+    password = models.CharField(max_length=100,blank=True,null=True)
+    password2 = models.CharField(max_length=100,blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class fishermen(models.Model):
+    name = models.TextField(blank=False)
+    email = models.EmailField(blank=True,null=True)
+    location= models.TextField(blank=True,null=True)
+    password = models.CharField(max_length=100,blank=True,null=True)
+    password2 = models.CharField(max_length=100,blank=True,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class fish1(models.Model):
+    user = models.ForeignKey(retailers,on_delete=models.CASCADE)
+    name = models.TextField(blank=False)
+    price = models.IntegerField(blank=False)
+    description = models.TextField(blank=True,null=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class fish2(models.Model):
+    user = models.ForeignKey(fishermen,on_delete=models.CASCADE)
+    name = models.TextField(blank=False)
+    price = models.IntegerField(blank=False)
+    description = models.TextField(blank=True,null=True)
+    
+    def __str__(self):
+        return self.name
